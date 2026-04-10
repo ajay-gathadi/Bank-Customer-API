bdb74e893da7661f078dfc3f90a493ab5decc4fb
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-10 16:06:20 +0530

    Add createCustomer method with the object data

--- src/main/java/com/bank/app/controller/CustomerController.java
+++ src/main/java/com/bank/app/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +19,12 @@ public class CustomerController {
     @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @PostMapping("/api/customers")
+    public String createCustomer(@RequestBody Customer customer){
+        String createdCustomer = customerService.createCustomer(customer);
+        return createdCustomer;
     }
 
     @GetMapping("/api/customers")
